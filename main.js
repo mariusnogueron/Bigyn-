bd17654e50850fd85ddd076ac793f55a602fa73d
Nogueron <marius.nogueron@gmail.com>
2024-10-24 23:10:54 +0200

    mixin button / projet fictif page

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import './style.scss'
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('miaou');
+})
+
+const buttonAccept = document.querySelector('.button__fictive-accept');
+
+buttonAccept.addEventListener('click', () => {
+    document.querySelector('.background__prevention').style.display = 'none';
+})
