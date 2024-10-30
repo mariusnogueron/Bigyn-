e92cc4af62e20366fe1180aa93af24be9c628dd4
Nogueron <marius.nogueron@gmail.com>
2024-10-30 23:03:45 +0100

    ajout images + arrow fonctionelle + grid relative images

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
 import './style.scss'
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('miaou');
-})
+
+
+const numberIteration = 6;
+const barContainer = document.querySelector('.transition__bar')
+
+
+for (let i = 0; i < numberIteration; i++) {
+
+    barContainer.forEach( `<div class = ${numberIteration} ></div>`);
+
+    if (i % 2 === 0) {
+        barContainer.style.backgroundColor = '$--pink-color';
+    } else {
+        barContainer.style.backgroundColor = '$--green-color';
+    }
+
+    barContainer.style.height = '100%';
+    barContainer.style.width = 'calc(100% / 6)';
+}
+
+
 
 const buttonAccept = document.querySelector('.button__fictive-accept');
 
 buttonAccept.addEventListener('click', () => {
     document.querySelector('.background__prevention').style.display = 'none';
 })
+
